7dea98ac79f4802cbf7af862409e324f09cf823b
yugj <yugj@homeking365.com>
2022-07-24 20:55:18 +0800

    disable tomcat overheadDataThreshold

--- nacos-service-demo/src/main/java/com/fm/demo/nacos/service/NacosServiceStarter.java
+++ nacos-service-demo/src/main/java/com/fm/demo/nacos/service/NacosServiceStarter.java
@@ -1,7 +1,11 @@
 package com.fm.demo.nacos.service;
 
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author yugj
@@ -13,6 +17,16 @@ public class NacosServiceStarter {
   public static void main(String[] args) {
     SpringApplication application = new SpringApplication(NacosServiceStarter.class);
     application.run(args).start();
+  }
+
+  @Bean
+  public ConfigurableServletWebServerFactory tomcatCustomizer() {
+    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
+    Http2Protocol protocol = new Http2Protocol();
+    protocol.setOverheadDataThreshold(0);
+    factory.addConnectorCustomizers(connector -> connector.addUpgradeProtocol(protocol));
+    return factory;
   }
 
 }
