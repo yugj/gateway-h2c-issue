8c011f2a2f11bcfd74b0ebdb0113ae8f036ebe28
yugj <yugj@homeking365.com>
2022-07-24 20:55:45 +0800

    - rollback

--- nacos-service-demo/src/main/java/com/fm/demo/nacos/service/NacosServiceStarter.java
+++ nacos-service-demo/src/main/java/com/fm/demo/nacos/service/NacosServiceStarter.java
@@ -1,11 +1,7 @@
 package com.fm.demo.nacos.service;
 
-import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author yugj
@@ -19,14 +15,14 @@ public class NacosServiceStarter {
     application.run(args).start();
   }
 
-  @Bean
-  public ConfigurableServletWebServerFactory tomcatCustomizer() {
-    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-
-    Http2Protocol protocol = new Http2Protocol();
-    protocol.setOverheadDataThreshold(0);
-    factory.addConnectorCustomizers(connector -> connector.addUpgradeProtocol(protocol));
-    return factory;
-  }
+//  @Bean
+//  public ConfigurableServletWebServerFactory tomcatCustomizer() {
+//    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//
+//    Http2Protocol protocol = new Http2Protocol();
+//    protocol.setOverheadDataThreshold(0);
+//    factory.addConnectorCustomizers(connector -> connector.addUpgradeProtocol(protocol));
+//    return factory;
+//  }
 
 }
